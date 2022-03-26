8b823d5b178f99160d32505f767e49f79f7c0ae3
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-26 14:14:05 +0600

    loaded data from json

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './shop.css'
 
 const Shop = () => {
+    const [products, setProducts]= useState([]);
+    useEffect(()=>{
+        fetch('watchDB.json')
+        .then(res=>res.json())
+        .then(data=> setProducts(data));
+    },[])
     return (
         <div className="shop-container">
            <div className="product-container">
-               <h2>Choose your Product from Here</h2>
+               <h3>Choose Your product from here</h3>
+               {
+                   products.map(product => <Product key={products.id}></Product>)
+               }
            </div>
            <div className="cart-container">
                <h2>Your order Details</h2>
